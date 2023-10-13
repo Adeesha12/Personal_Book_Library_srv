c32304f1cdcbcf05a2fe8bda758a57aad669e1cf
Adeesha <a3adeeshaanjana@gmail.com>
2023-10-13 11:26:02 +0530

    dockerize and add health check

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from typing import List, Optional
+from sqlalchemy.exc import OperationalError
+import time
 
 from database import engine
 import models
@@ -13,6 +15,10 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(books_router)
 app.include_router(register_router)
+
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=2)
